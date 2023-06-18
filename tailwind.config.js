e6ae9e4e4966e35c477a82eb7fe56338af38944d
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-18 11:45:38 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        black: "hsl(0,0,0)",
+      }
+    },
+  },
+  plugins: [],
+}
+
